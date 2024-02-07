b05e102a107bdc747eb3c9f772eaa1d320b96629
rahulyadav232 <ry6230198@gmail.com>
2024-02-07 18:49:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherDashboard from './components/weatherDashboard/WeatherDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherDashboard />
+    </div>
+  );
+}
+
+export default App;
